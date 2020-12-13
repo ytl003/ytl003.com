11009eea8de9c3f2c783f8331b9212bf641a040a
ytl003 <yanghua19970726@163.com>
2020-12-13 23:30:19 +0800

    start

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,8 +2,38 @@
  * @copyRight ytl003.com
  * @description
  */
+const { description } = require('../../package')
 module.exports = {
-  theme: 'reco',
+  plugins: ['@vuepress/pwa'],
   title: '小龙进阶',
-  description:'小龙进阶，大家一起学习一起进步'
+  description: description,
+  head: [
+    ['link', { rel: 'icon', href: '/logo.png' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['meta', { name: 'keywords', content: description }]
+  ],
+  theme: 'reco',
+  themeConfig: {
+    // 备案
+    record: 'ICP 备案文案',
+    recordLink: 'ICP 备案指向链接',
+    // 项目开始时间，只填写年份
+    startYear: '2020',
+    // 博客配置
+    blogConfig: {
+      category: {
+        location: 2,     // 在导航栏菜单中所占的位置，默认2
+        text: 'Category' // 默认文案 “分类”
+      },
+      tag: {
+        location: 3,     // 在导航栏菜单中所占的位置，默认3
+        text: 'Tag'      // 默认文案 “标签”
+      },
+      socialLinks: [     // 信息栏展示社交信息
+        { icon: 'reco-github', link: 'https://github.com/recoluan' },
+        { icon: 'reco-npm', link: 'https://www.npmjs.com/~reco_luan' }
+      ]
+    }
+  }
 }
---
+++ docs/things/blog-by-vuepress.md
@@ -0,0 +1,22 @@
+---
+title: 【vuepress】一款简洁而优雅的博客 & 文档静态网站生成器
+date: 2020-12-13
+categories:
+ - vue
+tags:
+ - vue
+ - blog
+---
+```
+# 安装
+yarn global add vuepress # 或者：npm install -g vuepress
+
+# 新建一个 markdown 文件
+echo '# Hello VuePress!' > README.md
+
+# 开始写作
+vuepress dev .
+
+# 构建静态文件
+vuepress build .
+```
