84f6c142b7ddd3a6320a192faeaa04e1515db455
ytl003 <yanghua19970726@163.com>
2020-12-15 23:05:45 +0800

    publish

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -20,6 +20,7 @@ module.exports = {
   },
   theme: 'reco',
   themeConfig: {
+    subSidebar: 'auto',
     //代码主题
     codeTheme: 'default',
     // 404公益
@@ -40,24 +41,23 @@ module.exports = {
     // 博客配置
     blogConfig: {
       category: {
-        location: 2,     // 在导航栏菜单中所占的位置，默认2
-        text: '分类' // 默认文案 “分类”
+        location: 2,
+        text: '分类'
       },
       tag: {
-        location: 3,     // 在导航栏菜单中所占的位置，默认3
-        text: '标签'      // 默认文案 “标签”
+        location: 3,
+        text: '标签'
       },
       socialLinks: [     // 信息栏展示社交信息
-        { icon: 'reco-github', link: 'https://github.com/recoluan' },
-        { icon: 'reco-npm', link: 'https://www.npmjs.com/~reco_luan' }
+        { icon: 'github', link: 'https://github.com/ytl003' }
       ]
     },
     nav: [
       { text: '首页', link: '/' },
-      { text: '生活', link: '/life/' },
+      { text: '生活', link: '/life/', icon: 'reco-suggestion' },
       { text: '时间轴', link: '/timeline/', icon: 'reco-date' },
-      { text: '留言', link: '/message/' },
-      { text: '关于', link: '/about/' }
+      { text: '留言', link: '/message/', icon: 'reco-message' },
+      { text: '关于', link: '/about/', icon: 'reco-other' }
     ],
     // type: 'blog',
     friendLink: [
@@ -73,7 +73,6 @@ module.exports = {
         email: 'recoluan@qq.com',
         link: 'https://www.recoluan.com'
       }
-      // ...
     ]
   }
 }
